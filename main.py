b5115fd96ad08a29e084779942888cf74d8a9a26
Nicolás Mariángel <cubemasterr@gmail.com>
2025-11-08 23:38:00 -0300

    just more parsing - renameGames

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from core.data import get_roms_data
 from core.statistics import create_statistics
 from core.duplicates import delete_duplicates
+from core.rename import rename_games
 from core.summary import create_summary
 from cli.parser import get_args
 
@@ -16,7 +17,10 @@ def main():
     data = get_roms_data(path)
 
     if args.deleteDuplicates:
-        delete_duplicates(path, data["games_and_consoles"])
+        delete_duplicates(path, data["games_and_consoles"], args.force)
+
+    if args.renameGames:
+        rename_games(path)
 
     if args.summary:
         create_summary(path)
