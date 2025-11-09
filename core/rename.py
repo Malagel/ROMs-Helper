8ba4fe8f711b078b49ecec25825509c01d2e4a57
Nicolás Mariángel <cubemasterr@gmail.com>
2025-11-09 19:11:12 -0300

    removed '-' from the regex renaming

--- core/rename.py
+++ core/rename.py
@@ -5,7 +5,7 @@ import re
 
 def clean_string(string: str) -> str:
     string = re.sub(r"\s*\([^)]*\)", '', string)
-    string = re.sub(r"[-_]+", ' ', string)
+    string = re.sub(r"[_]+", ' ', string)
     string = re.sub(r"v\d+(\.\d+)*", '', string)
 
     return string.strip()
@@ -15,15 +15,17 @@ def rename_file(file: Path, logs: bool) -> None:
     old_name = file.name
     new_name = file.with_name(clean_string(file.name))
 
-    try:
-        file.rename(new_name)
-        if logs: log(f"RENAMING TOOL: Renamed {old_name} -> {new_name.name}")
-    except FileExistsError:
-        if logs: log(f"RENAMING TOOL: Skipped {old_name}: {new_name.name} already exists in {file.parent}")
+    if old_name != new_name.name:
+        try:
+            file.rename(new_name)
+            if logs: log(f"[RENAMING TOOL]: Renamed {old_name} -> {new_name.name}")
+        except FileExistsError:
+            print(f"ERROR: Renaming caused a file to have the same name as other in the same folder. Skipping {old_name} from {file.parent}")
+            if logs: log(f"[RENAMING TOOL]: Skipped {old_name}: {new_name.name} already exists in {file.parent}")
 
 
 def rename_games(path: Path, logs: bool) -> None:
-    print("Renaming all your gamefiles...")
+    print("Renaming all your gamefiles... ", end="")
     for console in path.glob("*"): 
         if not console.is_dir(): continue
 
@@ -34,5 +36,5 @@ def rename_games(path: Path, logs: bool) -> None:
             else:
                 rename_file(sub, logs)
                 
-    print("All your games have been renamed and cleaned.")
+    print("DONE")
                 
