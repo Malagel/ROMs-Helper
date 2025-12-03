d0a1ebdcc680c4ed5751cd079d86c7be2d3c3108
Nicolás Mariángel <cubemasterr@gmail.com>
2025-12-03 18:00:09 -0300

    added more information to statistics and removed duplication statistics

--- core/statistics.py
+++ core/statistics.py
@@ -4,23 +4,17 @@ from core.utils import log
 def create_statistics(data: dict[str, dict], logs: bool) -> None:
     games_per_console = data["games_per_console"]
     gb_per_console = data["gb_per_console"]
-    games_and_consoles = data["games_and_consoles"]
+    games_data = data["games_data"]
 
     if logs: log(f"[STATISTICS TOOL]: Starting computation of data.")
 
-    print("Creating statistics from your collection... ", end="")
+    print("Creating statistics from your collection... ", end="", flush=True)
     with open("statistics.txt", "w") as f:
-        duplicates = {game: consoles for game, consoles in games_and_consoles.items() if len(consoles) > 1}
 
-        f.write("========== STATISTICS ==========\n\n")
+        f.write("============== STATISTICS ==============\n\n")
         f.write(f"Total Games: {sum(games_per_console.values())}\n")
         f.write(f"Total Size: {sum(gb_per_console.values())} GB\n")
         f.write(f"Consoles Analyzed: {len(games_per_console)}\n")
-
-        if duplicates:
-            f.write(f"Total Duplicates Found: {len(duplicates)}\n")
-        else:
-            f.write("No duplicates in your games found.\n")
 
         f.write(f"\nGames per console in descending order:\n\n")
         for i, (console, games) in enumerate(sorted(games_per_console.items(), key=lambda x: x[1], reverse=True), start=1):
@@ -30,10 +24,17 @@ def create_statistics(data: dict[str, dict], logs: bool) -> None:
         for i, (console, size) in enumerate(sorted(gb_per_console.items(), key=lambda x: x[1], reverse=True), start=1):
             f.write(f"{i}) {console}: {size} GB\n")
 
-        if duplicates:
-            f.write(f"\n\nDuplicates across consoles with identical name:\n\n")
-            for game, consoles in duplicates.items():
-                f.write(f"- {game} -> {', '.join(sorted(consoles))}\n")
+            console_games = [game for _, game in games_data.items() if game["metadata"]["console"] == console]
+
+            if not console_games: 
+                continue
+            
+            biggest = max(console_games, key=lambda x: x["metadata"]["size"])
+            smallest = min(console_games, key=lambda x: x["metadata"]["size"])
+
+            f.write(f"  Biggest game: {biggest['original_name']} -> {biggest['metadata']['size']} MB\n")
+            f.write(f"  Smallest game: {smallest['original_name']} -> {smallest['metadata']['size']} MB\n\n")
+
     
     print("DONE")
     if logs: log(f"[STATISTICS TOOL]: Statistics created and saved on 'statistics.txt'.")
