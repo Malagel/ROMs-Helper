69b2fe33c440a79420805205641161f560401e09
Nicolás Mariángel <cubemasterr@gmail.com>
2025-12-29 20:11:45 -0300

    small clarity improvement for timestamps

--- core/logger.py
+++ core/logger.py
@@ -6,7 +6,7 @@ log_file = None
 
 def start_logging():
     global log_file
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 
     log_dir = get_app_base_dir() / "logs" 
     log_dir.mkdir(parents=True, exist_ok=True)
@@ -15,9 +15,11 @@ def start_logging():
 
     log_file = log_path.open("a", encoding="utf-8")
 
+
 def log(msg: str):
     log_file.write(f"{msg}\n")
     log_file.flush()
+
 
 def stop_logging():
     log_file.close()
