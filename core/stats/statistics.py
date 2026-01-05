a3dd17d46456dbb1fda67b980421ad740abd6104
Nicolás Mariángel <cubemasterr@gmail.com>
2026-01-05 00:50:26 -0300

    refactor: standardize title and section line lengths in statistics output

--- core/stats/statistics.py
+++ core/stats/statistics.py
@@ -2,6 +2,8 @@ from core.utils import get_app_base_dir, format_bytes
 from datetime import datetime
 from core.logger import log
 
+TITLE_LINE = "=" * 90
+SECTION_LINE = "-" * 90
 
 def create_statistics(data: dict[str, dict], logs: bool) -> None:
     games_per_console = data["games_per_console"]
@@ -16,18 +18,15 @@ def create_statistics(data: dict[str, dict], logs: bool) -> None:
     now = datetime.now()
     statistics_path = base_dir / f"statistics_{now.strftime('%Y-%m-%d_%H-%M-%S')}.txt"
 
-    TITLE_LINE = "=" * 50
-    SECTION_LINE = "-" * 50
-
     total_games = sum(games_per_console.values())
     total_size_formatted = format_bytes(sum(bytes_per_console.values()))
     total_consoles = len(games_per_console)
 
-    print("Creating statistics from your collection... ", end="", flush=True)
+    print("• Creating statistics from your collection... ", end="", flush=True)
     with open(statistics_path, "w") as f:
 
         f.write(f"{TITLE_LINE}\n")
-        f.write("                ROMS STATISTICS\n")
+        f.write("                                      ROMS STATISTICS\n")
         f.write(f"{TITLE_LINE}\n\n")
 
         f.write(f"Generated on : {now.strftime('%d/%m/%Y at %H:%M:%S')}\n\n")
@@ -47,7 +46,7 @@ def create_statistics(data: dict[str, dict], logs: bool) -> None:
         f.write(f"{SECTION_LINE}\n")
 
         for i, (console, size) in enumerate(sorted(bytes_per_console.items(), key=lambda x: x[1], reverse=True), start=1):
-            f.write(f"[{i:02}] {console:<60} : {format_bytes(size):>10}\n")
+            f.write(f"[{i:02}] {console:<65} : {format_bytes(size):>10}\n")
 
             console_games = [game for _, game in games_data.items() if game["metadata"]["console"] == console]
 
@@ -58,17 +57,15 @@ def create_statistics(data: dict[str, dict], logs: bool) -> None:
             smallest = min(console_games, key=lambda x: x["metadata"]["size"])
 
             f.write(
-                f"      Biggest game  : {biggest['original_name']:<50} "
+                f"      Biggest game  : {biggest['original_name']:<55} "
                 f"→ {format_bytes(biggest['metadata']['size']):>10}\n"
             )
             f.write(
-                f"      Smallest game : {smallest['original_name']:<50} "
+                f"      Smallest game : {smallest['original_name']:<55} "
                 f"→ {format_bytes(smallest['metadata']['size']):>10}\n\n"
             )
 
-
-    
     print("DONE")
-    print("The file was created inside the 'statistics' folder.")
+    print("• The file was created inside the 'statistics' folder.")
     if logs: log(f"[STATISTICS TOOL]: Statistics created and saved on 'statistics_{now.strftime('%Y-%m-%d_%H-%M-%S')}.txt'.")
     
