a0a5c4239061cb95a6862b60100c90644376a07e
Nicolás Mariángel <cubemasterr@gmail.com>
2026-01-05 00:49:35 -0300

    refactor: enhance output formatting and improve user prompts in duplicate detection

--- core/duplicates.py
+++ core/duplicates.py
@@ -5,11 +5,13 @@ from itertools import combinations
 from rapidfuzz import fuzz
 from pathlib import Path
 import shutil
+import time
 
 MAX_CLUSTER_SIZE = 100
 
 class ClusterTooLargeError(Exception):
     pass
+
 
 def delete_game_paths(game_paths: list[Path], logs: bool, trueDeletion: bool) -> None:
     if not trueDeletion:
@@ -18,7 +20,7 @@ def delete_game_paths(game_paths: list[Path], logs: bool, trueDeletion: bool) ->
         delete_dir.mkdir(exist_ok=True)
 
     action = "Deleted" if trueDeletion else "Moved"
-    dest = "" if trueDeletion else "to 'DELETE' folder"
+    dest = "." if trueDeletion else " to 'DELETE' folder"
 
     for game_path in game_paths:
         is_dir = game_path.is_dir()
@@ -38,19 +40,21 @@ def delete_game_paths(game_paths: list[Path], logs: bool, trueDeletion: bool) ->
 
             shutil.move(str(game_path), str(target))
 
-        if logs: log(f"[DETECT DUPLICATES]: {action} {'folder' if is_dir else 'file'} {game_path} {dest}.")
+        if logs: log(f"[DETECT DUPLICATES]: {action} {'folder' if is_dir else 'file'} {game_path}{dest}")
 
-    print(f"{action} {len(game_paths)} games {dest}")
+    len_game_paths = len(game_paths)
+    game_s = 'game' if len_game_paths == 1 else 'games'
+    print(f"\n• {action} {len_game_paths} {game_s}{dest}")
 
 
 def confirm_delete(game_paths: list[Path], force: bool, trueDeletion: bool) -> bool:
     if force: return True
-
+    sentence = 'This game path' if len(game_paths) == 1 else 'These game paths'
     action = "moved to the 'DELETE' folder" if not trueDeletion else "deleted permanently"
-    print(f"\nThese game paths will be {action}:")
+    print(f"\n{sentence} will be {action}:")
 
     for p in game_paths:
-        print(f"- {p}")
+        print(f"▶ {p}")
 
     while True:
         print("\nDo you confirm? [y/N]")
@@ -87,6 +91,7 @@ def build_similarity_graph(games_data: dict[int, dict], threshold: float) -> dic
 
     return graph
 
+
 def dfs_clusters(graph: dict[int, list[int]], node: int, visited: set, cluster: list[int]):
     visited.add(node)
     cluster.append(node)
@@ -97,7 +102,6 @@ def dfs_clusters(graph: dict[int, list[int]], node: int, visited: set, cluster: 
     for neighbor in graph[node]:
         if neighbor not in visited:
             dfs_clusters(graph, neighbor, visited, cluster)
-
 
 
 def get_clusters(games_data: dict[int, dict], threshold: float, logs: bool) -> list[list[int]]:
@@ -120,7 +124,7 @@ def get_clusters(games_data: dict[int, dict], threshold: float, logs: bool) -> l
     
 
 def detect_duplicates(games_data: dict[int, dict], force: bool, logs: bool, threshold: float, trueDeletion: bool) -> None:
-    print(f"Building games with the threshold as {'default' if threshold == 76.0 else threshold}...", end="", flush=True)
+    print(f"• Building games with the threshold as {'default' if threshold == 76.0 else threshold}...", end="", flush=True)
 
     try:
         clusters = get_clusters(games_data, threshold, logs)
@@ -131,7 +135,7 @@ def detect_duplicates(games_data: dict[int, dict], force: bool, logs: bool, thre
         print(msg)
         if logs: log(msg)
 
-        print("\nDuplicates detection finalized.")
+        print("\n• Duplicates detection finalized.")
         return
 
     print("DONE")
@@ -149,7 +153,7 @@ def detect_duplicates(games_data: dict[int, dict], force: bool, logs: bool, thre
 
         print(f"Found {len(sorted_cluster)} games with the threshold as {'default' if threshold == 76.0 else threshold}:\n")
         for i, id in enumerate(sorted_cluster, start=1):
-            print(f"{i}) {games_data[id]['original_name']} -> {games_data[id]['metadata']['console']}")
+            print(f"[{i}] {games_data[id]['original_name']}  →  {games_data[id]['metadata']['console']}")
         
         while True:
             print(
@@ -170,16 +174,16 @@ def detect_duplicates(games_data: dict[int, dict], force: bool, logs: bool, thre
             except (ValueError, IndexError):
                 print(f"[ERROR]: The value is invalid, try again")
                 continue
-            break
+
+            if confirm_delete(game_paths, force, trueDeletion): 
+                delete_game_paths(game_paths, logs, trueDeletion)
+                break
+            else:
+                print("\n• Retrying...")
 
         if not choice: continue
         elif choice == 'quit': break
 
-        if confirm_delete(game_paths, force, trueDeletion): 
-            delete_game_paths(game_paths, logs, trueDeletion)
-        else:
-            print("Skipping...")
-            
         if not prompt_continue(): break
     
-    print("\nDuplicates detection finalized.")
+    print("\n• Duplicates detection finalized.")
